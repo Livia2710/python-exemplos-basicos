58d09cefc14471d67070b531a7be985660f9f58c
@Livia2710 <livia.sesi166@gmail.com>
2024-08-16 10:06:36 -0300

    f

---
+++ Python_03/07_while_senha.py
@@ -0,0 +1,17 @@
+# Reset contador e limite de tentativas
+i = 1
+while i <= 3:
+    user = input("Informe o usuário: ")
+    senha = input("Informe a senha: ")
+
+    # Checagem
+    if user != "Gisele" and senha != "123":
+        print("Usuário ou Senha Incorreto!")
+        print(" ")
+        i += 1
+    else:
+        print(" ")
+        print(f"Bem-Vindo, {user}!")
+        break
+else:
+    print(f"Você execedeu todas as: {i-1} tentativas !!")
